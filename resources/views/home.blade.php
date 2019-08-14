f37a626a70030ee44c3d4db05b632bcacbea1122
Ion <19718355+Nei-V@users.noreply.github.com>
2019-08-14 19:49:18 +0300

    added username, changing routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-3 p-5">
             <img src="https://instagram.fotp1-2.fna.fbcdn.net/vp/3658f2a85a43d34430b5d91474da3078/5DF066C8/t51.2885-19/s320x320/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fotp1-2.fna.fbcdn.net"
-                alt="sample logo" class="rounded-circle" style="height:200px" >
+                alt="sample logo" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div>
@@ -19,6 +19,20 @@
             <div class="font-weight-bold pt-4">Instagram Clone</div>
             <div>A clone of Instagram freeCodeCamp page made after a tutorial. This clone is made by </div>
             <div><a href="https://www.ionvarsescu.tk">Ion Varsescu</a></div>
+        </div>
+    </div>
+    <div class="row pt-10">
+        <div class="col-4">
+            <img src="https://instagram.fotp1-2.fna.fbcdn.net/vp/6be5d29f6b7f5c59a7842e15760f6e71/5DE9D44A/t51.2885-15/e35/c0.2.751.751a/s320x320/69095403_212234716426916_3447102072665452317_n.jpg?_nc_ht=instagram.fotp1-2.fna.fbcdn.net"
+                class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fotp1-2.fna.fbcdn.net/vp/62d08f4b842edcff9d86388c37dfa8da/5DEA477F/t51.2885-15/e35/c0.107.925.925a/s240x240/61555594_471501460067844_6848849947845320953_n.jpg?_nc_ht=instagram.fotp1-2.fna.fbcdn.net"
+                class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fotp1-2.fna.fbcdn.net/vp/28974f3602c1c17142ed9813a8ed7b8a/5DEFF206/t51.2885-15/e35/c0.81.887.887/s240x240/66420383_162375928225341_762635906960426989_n.jpg?_nc_ht=instagram.fotp1-2.fna.fbcdn.net"
+                class="w-100">
         </div>
     </div>
 </div>
